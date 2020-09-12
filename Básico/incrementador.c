44590a04bdcd6d8b0b06e8deec2ba3b78f9e2472
mautoz <mauricioozaki@gmail.com>
2020-09-12 18:46:15 -0300

    Reestruturando o repositório. Adicionando conceitos básicos.

---
+++ Básico/incrementador.c
@@ -0,0 +1,43 @@
+/**********************************************/
+/* Lista sem nó cabeça com inserção no início */
+/**********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    printf(">> Incremento\n");
+
+    int i = 0;
+
+    // Quando usamos o i++, ele primero realiza a tarefa antes
+    // do i = i + 1 e depois faz o incremento.
+    printf("%d\n", i++);
+    printf("%d\n\n", i);    
+
+    // O ++i é o contrário, primeiro realiza o i = i + 1 e depois
+    // faz a tarefa seguinte.
+    printf("%d\n", ++i);
+    printf("%d\n\n", i);    
+
+    // Esse dica simples é útil para deixar o código mais enxuto
+    // em situações como alterar uma posição em arranjo.
+    // Ao invés de fazer:
+    // A[i] = x;
+    // i++;
+    // Trocamos por:
+    // A[i++] = x;
+
+    // Para o decremento a regra é a mesma
+    printf(">> Decremento\n");
+
+    int j = 0;
+
+    printf("%d\n", j--);
+    printf("%d\n\n", j);
+
+    printf("%d\n", --j);
+    printf("%d\n\n", j);
+
+    return 0;
+}
---
+++ Básico/lacos.c
@@ -0,0 +1,42 @@
+/**********************************************/
+/* Lista sem nó cabeça com inserção no início */
+/**********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int i;
+    printf(">> Imprimindo de 1 a 10!\n\n");
+
+    // Básico, igual ao que vocês devem ter visto no JAVA
+    
+    printf("> For: \n");
+    
+    for(i = 0; i < 10; i++)
+        printf("%d ", (i+1));
+    printf("\n\n");
+
+
+    printf("> While: \n");
+
+    // Mesma ideia do for, com o tempo vocês decidirão quando
+    // um é melhor que o outro. 
+    i = 1;
+    while (i <= 10)
+        printf("%d ", i++);
+    printf("\n\n"); 
+
+
+    printf("> Do/While: \n");
+
+    // Parecido com o while, mas o while testa a condição desde
+    // o início, já o Do executa a primeira e só depois começa a testar.
+    i = 1;
+    do {
+        printf("%d ", i++);
+    } while (i <= 10);
+    printf("\n");
+
+    return 0;
+}
